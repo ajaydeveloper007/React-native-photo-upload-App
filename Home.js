2ca5d4c68f3ab3154992b7ea0d80a964f59db025
Ajay Kumar Yadav <32960809+ajaydeveloper007@users.noreply.github.com>
2024-08-06 22:43:47 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,21 @@
+import {Text, View } from "react-native";
+import SvgComponent from "../assets/SVG"
+
+export default function Home() {
+    return (
+        <View style = {{ flex:1}}>
+            <EmptyState />
+                
+        </View>
+    )
+}
+
+
+
+
+
+
+
+
+
+
